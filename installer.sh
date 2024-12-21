622075ccd0816d21b88f6ffddf918e65655bc278
Pavel Čief <35612046+ciefp@users.noreply.github.com>
2024-12-21 17:19:59 +0100

    update installer

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@ version='1.1'
 changelog='\nFix little bugs\nUpdated Picons List'
 ##############################################################
 
-TMPPATH=/tmp/CiefpsettingsMotor
+TMPPATH=/tmp/CiefpWhitelistStreamrelay
 
 if [ ! -d /usr/lib64 ]; then
 	PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/CiefpWhitelistStreamrelay
